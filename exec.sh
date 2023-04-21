32e6715a392604c9629e3d3f69f574919b9a715f
Luis Urban <lurbanrivero@gmail.com>
2023-04-21 00:37:44 -0600

    Update all files

---
+++ exec.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+"python3" "timetabling_v2_pulp.py" "instancia/2022-3/upmh-2022-3_materias.csv" "instancia/2022-3/upmh-2022-3_profesores.csv" "instancia/2022-3/upmh-2022-3_fijos.csv" "instancia/2022-3/upmh-2022-3_turnos.csv" "upmh-2022-3"
+#"python3" "timetabling_cap_pulp_v1.py" "instancia/2022-3/upmh-2022-3_out.csv" "instancia/2022-3/upmh-2022-3_grupos.csv" "instancia/2022-3/upmh-2022-3_salones.csv" "upmh-2023-3"
+
